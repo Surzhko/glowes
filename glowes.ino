5c8de5c3d4c7d7c1ae87698a3cf1d1fe13c48140
Eugene Surzhko <SurzhkoYevhen@gmail.com>
2018-03-28 00:56:03 +0300

    added BMP180 library

--- glowes.ino
+++ glowes.ino
@@ -1,46 +1,30 @@
 #include <Wire.h>
+#include "BMP180.h"
 
 const byte TEMPERATURE_AND_HUMIDITY_SENSOR_ID     = 0x40;
 const byte PRESURE_SENSOR_ID                      = 0x77;
 
 const byte TRIGGER_TEMPERATURE_MEASUREMENT_HOLD   = 0xE3;
 const byte TRIGGER_HUMIDITY_MEASUREMENT_HOLD      = 0xE5;
-const byte TRIGGER_PRESURE_MEASUREMENT_HOLD       = 0xF6;
 const byte TRIGGER_TEMPERATURE_MEASUREMENT_NOHOLD = 0xF3;
 const byte TRIGGER_HUMIDITI_MEASUREMENT_NOHOLD    = 0xF5;
+
+BMP180 Bmp;
 
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-
-  Wire.beginTransmission(PRESURE_SENSOR_ID);
-  Wire.write(0xAA);
-  byte status = Wire.endTransmission();
-  Serial.print("Status: ");
-  Serial.println(status);
-
-  byte amount = Wire.requestFrom(PRESURE_SENSOR_ID, 2);
-  Serial.print("Amount of read bytes: ");
-  Serial.println(amount);
-
-  byte msb, lsb;
-
-  msb = Wire.read();
-  lsb = Wire.read();
-  uint16_t rawTemperature = ((uint16_t) msb << 8) | (uint16_t) lsb;
-  Serial.println(rawTemperature);
-  
+  Bmp.init();
 }
 
-void loop() {
-  // TEMPERATURE
+void measure_temperature() {
   Wire.beginTransmission(TEMPERATURE_AND_HUMIDITY_SENSOR_ID);
   Wire.write(TRIGGER_TEMPERATURE_MEASUREMENT_HOLD);
   byte status = Wire.endTransmission();
-  Serial.print("Status: ");
-  Serial.println(status);
+//  Serial.print("Status: ");
+//  Serial.println(status);
 
-  byte amount = Wire.requestFrom(TEMPERATURE_AND_HUMIDITY_SENSOR_ID, 3);
+  byte amount = Wire.requestFrom(TEMPERATURE_AND_HUMIDITY_SENSOR_ID, (uint8_t)3);
 
 //  Serial.print("Amount of read bytes: ");
 //  Serial.println(amount);
@@ -58,13 +42,14 @@ void loop() {
     Serial.print(realTemperature);
     Serial.print("C, ");
   }
+}
 
-  // HUMIDITY
+void measure_humidity() {
   Wire.beginTransmission(TEMPERATURE_AND_HUMIDITY_SENSOR_ID);
   Wire.write(TRIGGER_HUMIDITY_MEASUREMENT_HOLD);
-  status = Wire.endTransmission();
+  byte status = Wire.endTransmission();
 
-  amount = Wire.requestFrom(TEMPERATURE_AND_HUMIDITY_SENSOR_ID, 3);
+  byte amount = Wire.requestFrom(TEMPERATURE_AND_HUMIDITY_SENSOR_ID, (uint8_t)3);
 
 //  Serial.print("Amount of read bytes: ");
 //  Serial.println(amount);
@@ -82,33 +67,16 @@ void loop() {
     Serial.print(realTemperature);
     Serial.print("% ");
   }
+}
 
+void loop() {
+  Serial.print(Bmp.getPressure());
+  Serial.print("KPa, ");
+  Serial.print(7.50062 * Bmp.getPressure());
+  Serial.print("mm, ");
 
-  // PRESURE
-  Wire.beginTransmission(PRESURE_SENSOR_ID);
-  Wire.write(TRIGGER_PRESURE_MEASUREMENT_HOLD);
-  status = Wire.endTransmission();
-//  Serial.print("Status: ");
-//  Serial.println(status);
-
-  amount = Wire.requestFrom(PRESURE_SENSOR_ID, 3);
-
-//  Serial.print("Amount of read bytes: ");
-//  Serial.println(amount);
-
-  if (amount == 3) {
-    byte msb, lsb, checksum;
-  
-    msb = Wire.read();
-    lsb = Wire.read();
-    checksum = Wire.read();
-  
-    uint16_t rawTemperature = ((uint16_t) msb << 8) | (uint16_t) lsb;
-    float tempTemperature = rawTemperature * (125.0 / 65536.0); //2^16 = 65536
-    float realTemperature = tempTemperature - 6.0; //From page 14
-    Serial.print(rawTemperature);
-    Serial.print("P ");
-  }
+  measure_temperature();
+  measure_humidity();
 
   Serial.println("==================");
   delay(2000);
